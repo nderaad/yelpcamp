950a05b7534176e8efa67076315528b6034aeedd
Nick <username@users.noreply.github.com>
2017-04-13 21:56:34 -0400

    setup npm and dependencies

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
